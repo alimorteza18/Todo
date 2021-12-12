787b0e90ac868974f2b5efa9aa3fcfb576ffa5bf
Darkness <alimurtaza7x@gmail.com>
2021-12-12 10:02:07 +0330

    Updates

---
+++ precache-manifest.978049fe8ac9747fe6c2f828fbc99383.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8d07751882178b7ccc8",
+    "url": "/Todo/static/css/main.ad754ebf.chunk.css"
+  },
+  {
+    "revision": "a8d07751882178b7ccc8",
+    "url": "/Todo/static/js/main.a8d07751.chunk.js"
+  },
+  {
+    "revision": "46b5172da52bda208e40",
+    "url": "/Todo/static/js/1.46b5172d.chunk.js"
+  },
+  {
+    "revision": "79583ee1fd0424510277",
+    "url": "/Todo/static/js/runtime~main.79583ee1.js"
+  },
+  {
+    "revision": "07bc4445922203f393c352298c1331ab",
+    "url": "/Todo/index.html"
+  }
+];
